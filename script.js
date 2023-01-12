b530a34a247f43dc54c1c4c6559070a18dbcb6bf
shaxa_9505 <87860218+shaxa9505@users.noreply.github.com>
2023-01-12 20:24:02 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+"use strict";
+
+
+// function sayHelloWorld (text) {
+//   console.log("Hello world");
+//   console.log(text);
+// }
+
+// sayHelloWorld("Hello world");
+
+// function calc (a, b) {
+//   console.log(a + b);
+// }
+// calc(10, 5);
+
+
+
+
+// function sayMyName (name, surname) {
+//   var age = 30;
+//   return age wunde return qiganimiz bn bu uzgaruvchi baribir tawqariga ciqmedi
+//   console.log(`My name is ${name} ${surname}`);
+// }
+
+// sayMyName("Shokhrukh", "Meliboev");
+// console.log(age); 
+
+
+// let age = 20;
+
+// function show () {
+//   let age = 30;
+//   console.log(age);
+// }
+// show()
+// console.log(age);
+
+
+
+// function calc (a, b) {
+//   return a + b;
+//   console.log(a, b);
+// }
+
+// console.log(calc(1, 2));
+// const sumOfAandB = calc(10, 5);
+// console.log(sumOfAandB);
+
+
+// Functions 
+
+// 1-Function declaration
+foo()
+function foo () {
+  console.log("Function declaration");
+}
+
+
+// 2-Function expression
+let bar = function () {
+  console.log("Function expression");
+}
+bar()
+
+
+// 3-Arrow functions
+const calc = (a, b) => {
+  console.log("Arrow functions");
+}
+calc();
